3df84a07582c625af63b00aeea025cfbcf014088
Rupesh Mangal <rupesh.code@gmail.com>
2018-08-31 22:25:17 +0530

    deleted .env

--- try3.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, render_template
-import feedparser
-url = request.form['url']
-dic = {}
-url = 'https://www.google.co.in/search?&q=filetype%3Apdf+2014+india+election&num=100'
-data = feedparser.parse(url)
-print(data)
